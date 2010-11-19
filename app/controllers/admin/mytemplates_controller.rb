f30516d8d509451293c73796f212dbe37ab489a4
Hyun Soo Kim <cholubin@gmail.com>
2010-11-19 17:23:01 +0900

    update

--- app/controllers/admin/mytemplates_controller.rb
+++ app/controllers/admin/mytemplates_controller.rb
@@ -38,6 +38,8 @@ class Admin::MytemplatesController < ApplicationController
       @mytemplates = Mytemplate.all(:in_order => false, :order => [:created_at.desc]).fc_filter(feedback_code,job_code).search(params[:search],page)
       @total_count = Mytemplate.all(:in_order => false).search(params[:search],"").count
       @subtotal_count = @mytemplates.count
+      
+      puts_message 
     end
     
     @categories = Category.all(:order => :priority)    
